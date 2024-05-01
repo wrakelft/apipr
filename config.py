eb07d2d48425745ba005b2ab3408d4ae95e8e643
wrakelft <164265963+wrakelft@users.noreply.github.com>
2024-05-01 19:35:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 
 load_dotenv()
 
-username = os.environ.get("username")
-password = os.environ.get("password")
-host = os.environ.get("host")
-port = os.environ.get("port")
-database_name = os.environ.get("database_name")
+username = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASS")
+host = os.environ.get("DB_HOST")
+port = os.environ.get("DB_PORT")
+database_name = os.environ.get("DB_NAME")
+
