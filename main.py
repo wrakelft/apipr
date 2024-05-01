3649df07d187eed762ee412437479665075124d0
wrakelft <164265963+wrakelft@users.noreply.github.com>
2024-05-01 21:21:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def startup_event():
     await create_tables(engine)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True, workers=3)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=3)
 
 
 
